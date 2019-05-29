732a4c9608a676b8ebf03915c1a5203df16c996e
Chris Michael <chris.perez@upr.edu>
2019-05-29 11:32:23 -0400

    :sparkles: First commit!

---
+++ README.md
@@ -0,0 +1,23 @@
+# Vue Store Starter CLI
+💡 *A CLI that automatically clone the [vuex-store-starter](https://github.com/ChrisMichaelPerezSantiago/vuex-store-starter)  template* 
+
+
+## Installation
+Install the CLI globally OR use npx:
+```
+npm install -g vuex-store-starter-cli
+```
+
+##Usage
+```
+# with global install
+vuex-store-starter-cli repo-name
+# with npx
+vuex-store-starter-cli repo-name
+```
+
+
+
+## Where to get Help
+If you have any questions about the template, contact me at email chrisperezsantiago1@gmail.com
+I'll be glad you give this template a good use! 💖
---
+++ index.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  return console.log(`
+  Invalid directory name.
+  Usage: <vuex-store-starter-cli> <repo-name>  
+`);
+}
+
+const URL = 'https://github.com/ChrisMichaelPerezSantiago/vuex-store-starter.git';
+
+f('git', ['clone', URL, name])
+  .then(() => {
+    return f('rm', ['-rf', `${name}/.git`]);
+  }).then(() => {
+    console.log('Installing dependencies...');
+    return f('npm', ['install'], {
+      cwd: process.cwd() + '/' + name
+    });
+  }).then(() => {
+    console.log('Done! 🏁');
+    console.log('');
+    console.log('cd', name);
+    console.log('npm run start');
+    console.log('👑For more information check the package.json')
+  });
+
+function f(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+  return new Promise((resolve) => {
+    spawned.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+    spawned.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
